a12b0a57e18ccaeda9c9688941db33fe53f169c9
Kanwalme <mehmudkanwal@gmail.com>
2022-08-19 10:17:11 -0500

    WIP FindingsList

--- src/FindingsLists.java
+++ src/FindingsLists.java
@@ -1,5 +1,52 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class FindingsLists {
-    public FindingsLists(){
+    public FindingsLists() throws InterruptedException {
+    Thread.sleep(500);
+
+    System.out.println();
+
+    ArrayList<String> rockList = new ArrayList<String>();
+        System.out.println("Rock data downloaded");
+
+        rockList.add("rock");
+        rockList.add("weird rock");
+        rockList.add("smooth rock");
+        rockList.add("not rock");
+
+        System.out.println(rockList);
+
+        System.out.println("Wait a second that last one is not a rock we need to delete that one from the list" );
+        rockList.remove("not rock");
+        System.out.println(rockList);
+        System.out.println("Perfect");
+
+        Thread.sleep(500);
+
+        HashMap<String, String> fossilDirectory = new HashMap<>();
+        System.out.println("Fossil data downloaded");
+
+        fossilDirectory.put("Bird Fossil", "The fossil has wings implying it as capable of flight ");
+        fossilDirectory.put("Fish Fossil", "The fossil is vaguely fish shaped implies there was once water");
+        fossilDirectory.put("Tooth Fossil", "The tooth from an unknown fossil");
+
+        System.out.println("Which of the fossils would you like to learn more about (Spelling and Spacing is important)");
+        Scanner input = new Scanner(System.in);
+
+        String fossilChoice = input.nextLine();
+
+        if(fossilChoice.equalsIgnoreCase("Bird Fossil")){
+            System.out.println("Fossil: " + fossilChoice + "\nDescription: " + fossilDirectory.get(fossilChoice));
+        } else if (fossilChoice.equalsIgnoreCase("Fish Fossil")){
+            System.out.println("Fossil: " + fossilChoice + "\nDescription: " + fossilDirectory.get(fossilChoice));
+        } else if (fossilChoice.equalsIgnoreCase("Tooth Fossil")){
+            System.out.println("Fossil: " + fossilChoice + "\nDescription: " + fossilDirectory.get(fossilChoice));
+        }
+
+
+
 
     }
 }
